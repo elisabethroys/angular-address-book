71c9de1d8bef4d524a179ba640fc24421ca19579
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-09-08 11:12:49 +0200

    Added three modules to contacts, and set up routing to add and list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './contacts/list/list.component';
+import { AddComponent } from './contacts/add/add.component';
+
+const routes: Routes = [
+  { path: 'contacts', component: ListComponent},
+  { path: 'contacts/add', component: AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
